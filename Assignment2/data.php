fe9e5fdf7e819a5ea51455e62457661c6e2787d2
Blake Wagner <blakewagner828@outlook.com>
2021-09-09 18:33:20 -0400

    Assignment2

---
+++ Assignment2/data.php
@@ -0,0 +1,56 @@
+<?php
+			$students = array(
+				
+					[
+						"name" => "Blake",
+						"major" => "Cybersecurity",
+						"year" => "sophomore",
+						"skills" => [ 
+							"Java" => "70",
+							"HTML" => "70",
+							"Linux" => "60",
+							"Windows" => "60",
+							],
+						"company" => "FBI",
+						"picture" => 'https://bootdey.com/img/Content/avatar/avatar1.png',
+						],
+						
+					[
+						"name" => "Nazrul",
+						"major" => "CIT",
+						"year" => "junior", 
+						"skills" => [ 
+							"Linux" => "95",
+							"Windows" => "75",
+							"Apple" => "60",
+							],
+						"company" => "Apple",
+						"picture" => 'https://bootdey.com/img/Content/avatar/avatar7.png',
+						],
+						
+					[
+						"name" => "Riyadh",
+						"major" => "Computer Science",
+						"year" => "freshman",
+						"skills" => [ 
+							"Java" => "80",
+							"Python" => "75",
+							"C++" => "60",
+							],
+						"company" => "Google",
+						"picture" => "https://bootdey.com/img/Content/avatar/avatar6.png",
+						],
+					[
+						"name" => "Niloy",
+						"major" => "Data Analyst",
+						"year" => "senior",
+						"skills" => [ 
+							"Java" => "75",
+							"PHP" => "75",
+							"Linux" => "75",
+							],
+						"company" => "Amazon",
+						"picture" => "https://bootdey.com/img/Content/avatar/avatar2.png",
+						],
+				);
+?>
